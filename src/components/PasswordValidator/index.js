ca9971d377f4c3d1654f7ca648f22a90d9a8f98e
ganeshparuchuri <ganesh3paruchuri@gmail.com>
2023-03-09 07:55:27 +0000

    added-functionality

--- src/components/PasswordValidator/index.js
+++ src/components/PasswordValidator/index.js
@@ -1,14 +1,38 @@
 // Write your code here
 import {useState} from 'react'
-import {Maincontainer, Passwordcontainer} from './styledComponents'
+import {
+  Maincontainer,
+  Passwordcontainer,
+  PasswordHeading,
+  Passwordparagraph,
+  Inputfield,
+  Warningmsg,
+} from './styledComponents'
 
 const PasswordValidator = () => {
   const [inputPassword, setInputPassword] = useState('')
 
+  const onchangeInput = event => {
+    setInputPassword(event.target.value)
+  }
+
   return (
     <Maincontainer>
       <Passwordcontainer>
-        <p>Password Validator</p>
+        <PasswordHeading>Password Validator</PasswordHeading>
+        <Passwordparagraph>
+          Check how strong and secure is your password
+        </Passwordparagraph>
+        <Inputfield
+          type="password"
+          onChange={onchangeInput}
+          value={inputPassword}
+        />
+        {inputPassword.length < 8 ? (
+          <Warningmsg>Your password must be at least 8 characters</Warningmsg>
+        ) : (
+          ''
+        )}
       </Passwordcontainer>
     </Maincontainer>
   )
